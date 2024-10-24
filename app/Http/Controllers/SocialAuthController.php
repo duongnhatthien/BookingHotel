fc81b44a649f3ad45ef390e0a872e39c6cf4cc29
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-10-24 19:23:32 +0700

    Authentication
    complete authentication system with social login and password reset, password update, verify email,  login, register

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Password;
+
+class SocialAuthController extends Controller
+{
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $socialUser = Socialite::driver($provider)->user();
+        $user = User::updateOrCreate(
+            [
+                'email' => $socialUser->getEmail(),
+            ]
+            ,
+            [
+                'name' => $socialUser->getName(),
+                'email' => $socialUser->getEmail(),
+                'email_verified_at' => now(),
+                'password' => null,
+            ]
+        );
+        Auth::login($user);
+        if ($user->markEmailAsVerified()) {
+            event(new Verified($user));
+        }
+        $token = Password::createToken(Auth::user());
+        return redirect()->route('password.reset', ['token' => $token, 'email' => $user->email]);
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -16,6 +17,13 @@ Route::middleware('guest')->group(function () {
         ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/auth/redirect/{provider}', [SocialAuthController::class, 'redirectToProvider'])
+        ->where('provider', 'github|google|facebook')->name('social.redirect');
+
+    Route::get('/auth/callback/{provider}', [SocialAuthController::class, 'handleProviderCallback'])
+        ->where('provider', 'github|google|facebook')->name('social.callback');
+
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -35,7 +43,7 @@ Route::middleware('guest')->group(function () {
         ->name('password.store');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
